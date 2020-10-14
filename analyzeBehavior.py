b615467b8091a8806bd9abd6c01f5153bc4ff666
acumpelik <acumpelik@gmail.com>
2020-10-14 12:26:38 +0200

    added Jupyter notebooks
    also updated data csv files to include Boolean (T/F to 1/0 for trial correct)

--- analyzeBehavior.py
+++ analyzeBehavior.py
@@ -10,12 +10,12 @@ JC240_data.info() # Index, data type, and memory information
 
 
 # Split the data by Session ID
-numTrials = JC240_data.groupby("Session_ID").size() # lists the number of trials per session
-sessionID = JC240_data["Session_ID"].unique() # returns an array of unique session IDs
-session1 = JC240_data.loc[JC240_data.Session_ID==1]
+# numTrials = JC240_data.groupby("Session_ID").size() # lists the number of trials per session
+# sessionID = JC240_data["Session_ID"].unique() # returns an array of unique session IDs
+# session1 = JC240_data.loc[JC240_data.Session_ID==1]
 
 # trialOutcome =
 
-numCorrectTrials = JC240_data.groupby("SessionID")
+# numCorrectTrials = JC240_data.groupby("SessionID")
 
 # apply(list)
