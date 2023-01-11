4414fba45e018bf2aaf8b7b7f418d0693294f0d5
acumpelik <acumpelik@gmail.com>
2023-01-11 17:16:53 -0500

    replaced the error value with a NaN and plotted the new scatterplot, it looks as it should
    - many missing timestamps when the animal was
    - should quantify these timestamps (as a percentage, possibly also through time, e.g. show valid timestamps in blue and missing ones in red)
    - should bin them, hopefully that should get rid of most of the issues
    - down the line: plot over time

--- positionTracking.py
+++ positionTracking.py
@@ -1,6 +1,8 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
+# import the data
 f = np.loadtxt('JC274-20220312.whl')
 x1 = f[:,0] # primary LED
 y1 = f[:,1] # primary LED
@@ -9,10 +11,15 @@ y2 = f[:,3] # secondary LED, inactive
 timestamps = f[:,4]
 valid = f[:,5] # binary that indicates if record is valid or not
 
-print(timestamps)
+###
+
+# convert to df; first make a dictionary, then convert
+df_dict = {"x1": x1, "y1": y1,"x2": x2, "y2": y2, "timestamps": timestamps, "valid": valid}
+df = pd.DataFrame(df_dict)
+df[df == 1023] = np.nan # replace 1023 (error value) with NaN
+print(df.describe())
 
 ###
 
-plt.scatter(x1, y1, alpha=0.5)
+plt.scatter(df.loc[:,"x1"], df.loc[:,"y1"], alpha=0.5)
 plt.show()
-
