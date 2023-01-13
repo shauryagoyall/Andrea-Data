ce2245c5274eedb0750d066fdf4a1903e0046f43
acumpelik <acumpelik@gmail.com>
2023-01-13 11:43:17 -0500

    made interactive figure in .py script

--- positionTracking.py
+++ positionTracking.py
@@ -1,25 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## JC274-20220314 .whl metadata
+# Animal: **JC274** <br>
+# Chocolate_arm: **8** <br>
+# Honey_arm: **4** <br>
+# Sunflower_arm: 5 (not relevant for this session) <br>
+# ---
+# Training_day: **8** <br>
+# Accuracy: **64.29%** <br>
+# Threshold_reached_day: **10** <br>
+
+# In[3]:
+
 import numpy as np
 import pandas as pd
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import datetime
 
-# import the data
-f = np.loadtxt('JC274-20220312.whl')
-x1 = f[:,0] # primary LED
-y1 = f[:,1] # primary LED
-x2 = f[:,2] # secondary LED, inactive
-y2 = f[:,3] # secondary LED, inactive
-timestamps = f[:,4]
-valid = f[:,5] # binary that indicates if record is valid or not
+import IPython
+print(IPython.sys_info())
+# mpl.use('Qt5Agg')
+mpl.use('TkAgg')
 
-###
+# plt.ion()
 
-# convert to df; first make a dictionary, then convert
-df_dict = {"x1": x1, "y1": y1,"x2": x2, "y2": y2, "timestamps": timestamps, "valid": valid}
-df = pd.DataFrame(df_dict)
-df[df == 1023] = np.nan # replace 1023 (error value) with NaN
-print(df.describe())
 
-###
+basename = 'JC274-20220314'
+timenow = datetime.datetime.today().strftime("%Y%m%d-%H%M%S")
 
-plt.scatter(df.loc[:,"x1"], df.loc[:,"y1"], alpha=0.5)
+pos = pd.read_csv(basename+'.whl', sep=" ",
+                 header=None,
+                 names=["x1","y1","x2","y2","timestamps","valid"])
+
+
+pos[pos == 1023] = np.nan # replace 1023 (error value) with NaN
+
+x1 = pos["x1"]
+y1 = pos["y1"]
+timestamps = pos["timestamps"]
+
+fig, ax = plt.subplots(figsize=(8,8))
+ax.plot(x1, y1, linewidth=1)
+ax.set_title(basename+" tracking", fontsize=18, y=1.01)
+ax.tick_params(labelsize=14)
+plt.xlim(0,160)
+plt.ylim(0,160)
 plt.show()
+# plt.savefig(basename+"_tracking_"+timenow+".png")
+
+# print(timestamps.min(), timestamps.max())
+# print(timestamps.count())
+
+# timestart = 0
+# timeend = 1_030_000
+# subset = pos.loc[(timestamps > timestart) & (timestamps < timeend)]
+#
+# fig, ax = plt.subplots(figsize=(8,8))
+# ax.plot(subset["x1"], subset["y1"], linewidth=1)
+# ax.set_title(basename+" tracking subset", fontsize=18, y=1.01)
+# ax.tick_params(labelsize=14)
+# plt.xlim(0,160)
+# plt.ylim(0,160)
+# plt.show()
+## plt.savefig(basename+"_tracking-subset_"+timenow+".png")
